535a12818658579fdf4c4e64b471560b79eeca45
Zulqarnain-AIgit config --global user.name Zulqarnain-AIgit config --global user.name Zulqarnain-AI <qarnainhassan786@gmail.com>
2025-12-03 10:55:20 +0500

    tuple in python and its methods

---
+++ python_tuple.py
@@ -0,0 +1,13 @@
+tpl=(1,2,3,4,5)
+
+print(f'length of tuple: {len(tpl)}')
+
+print(f'max of tuple: {max(tpl)}')
+
+print(f'min of tuple: {min(tpl)}')
+
+print(f'sum of tuple: {sum(tpl)}')
+
+print(f'index of an item in tuple: {tpl.index(4)}')
+
+print(f'count of an item in tuple: {tpl.count(4)}')
